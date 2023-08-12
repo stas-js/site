7dd242e8b8335f8a2b522c8da3949cef53daf67f
stas-js <100315104+stas-js@users.noreply.github.com>
2023-08-12 21:56:43 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,14 +1,56 @@
-var plusOne = function(digits) {
-for(var i = digits.length - 1; i >= 0; i--){
-     digits[i]++; 
-    if(digits[i] > 9){
-        digits[i] = 0;
-    }else{
-        return digits;
-    }
-}
-digits.unshift(1);
-return digits;
-};
+function allocateVAT() {
+        let beautyDigit = document.getElementById("input_price").value;
+        let cursorPosition = document.getElementById("input_price").selectionStart;
+        let strLength = beautyDigit.length;
+        let difference = strLength - cursorPosition;
+        let newCursorPosition;
 
-document.write(plusOne([2, 2, 3]))
+        beautyDigit = formatting(beautyDigit);
+        let newStrLength = beautyDigit.length;
+
+        switch (true) {
+            case difference === 0:
+                newCursorPosition = newStrLength;
+                break;
+
+            case newStrLength === strLength:
+                newCursorPosition = newStrLength - difference;
+                break;
+
+            case newStrLength > strLength:
+                let array = beautyDigit.split('')
+                if (array[cursorPosition] === " ") {
+                    difference = difference + 1
+                }
+                newCursorPosition = newStrLength - difference;
+                break;
+
+            case newStrLength < strLength:
+                newCursorPosition = newStrLength - difference;
+                if (newCursorPosition < 0) {
+                    newCursorPosition = 0
+                }
+
+                break;
+        }
+
+        document.getElementById("input_price").value = beautyDigit;
+        document.getElementById("input_price").setSelectionRange(newCursorPosition, newCursorPosition);
+
+        let input_price = toDigit(beautyDigit);
+        let input_VAT_percent = document.getElementById("input_VAT").value;
+        cursorPosition = document.getElementById("input_VAT").selectionStart;
+        let withPercent = addPercent(input_VAT_percent)
+        document.getElementById("input_VAT").value = withPercent;
+        document.getElementById("input_VAT").setSelectionRange(cursorPosition, cursorPosition);
+        input_VAT_percent = toDigit(input_VAT_percent)
+        let output_VAT = +(input_price * input_VAT_percent / (100 + input_VAT_percent)).toFixed(2);
+        let result = (input_price - output_VAT).toFixed(2);
+        result = result.replace(/\./, ',').replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+        output_VAT = output_VAT.toFixed(2).replace(/\./, ',').replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+        input_price = input_price.toFixed(2).replace(/\./, ',').replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+        document.getElementById("total-3").textContent = "НДС " + input_VAT_percent + " %:"
+        document.getElementById("total-4").textContent = result + " руб."
+        document.getElementById("total-5").textContent = output_VAT + " руб."
+        document.getElementById("total-6").textContent = input_price + " руб."
+    }
